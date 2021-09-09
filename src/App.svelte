440cf29c7f226d9777795b82b761769b9cc7056b
Matthew Barnden <development.esharmony@gmail.com>
2021-09-09 15:17:53 +0100

    tidy up

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-	import Counter, {CounterState} from './FuseTypes/Counter';
-	import Buttons from './Buttons.svelte'
-	let counterState: CounterState = Counter.state;
+  import Counter, { CounterState } from './FuseTypes/Counter';
+  import Buttons from './Buttons.svelte';
+  let counterState: CounterState = Counter.state;
 
-	const updateCounter = (state: CounterState) => {
-		counterState = state;
-	}
+  const updateCounter = (state: CounterState) => {
+    counterState = state;
+  };
 
-	Counter.register(updateCounter, "app");
-
+  Counter.register(updateCounter, 'app');
 </script>
 
 <section>
-	<p> {counterState.count}</p>
+  <p>{counterState.count}</p>
 </section>
 <Buttons />
 
 <style>
-	section {
-		width: '100%';
-		height: '100%';
-		text-align: center;
-	}
-	p {
-		font-size: x-large;
-	}
+  section {
+    width: '100%';
+    height: '100%';
+    text-align: center;
+  }
+  p {
+    font-size: x-large;
+  }
 </style>
-
-
